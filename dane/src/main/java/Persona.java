de7621b7f44037f8a4924b6f3249a8d642d6f0a4
JulianSCLlanos <julianstevencll@hotmail.com>
2023-07-21 00:32:44 -0500

    Corrección 1 Commit

--- dane/src/main/java/Persona.java
+++
@@ -1,9 +0,0 @@
-
-
-import jakarta.persistence.Entity;
-
-@Entity;
-
-public class Persona {
-    
-}
--- dane/src/main/java/com/dane/demo/controller/PersonaController.java
+++ dane/src/main/java/com/dane/demo/controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.dane.demo.model.Persona;
 import com.dane.demo.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
